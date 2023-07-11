986ee6af331e35a7a5cf64745a5079ab5dd2affe
shaiknabee <138884531+shaiknabee@users.noreply.github.com>
2023-07-11 17:36:09 +0530

    commit12
    updated values in code

--- GitPractice/src/conflictstest/addition.java
+++ GitPractice/src/conflictstest/addition.java
@@ -4,7 +4,7 @@ public class addition
 {
 	public static void main(String[] args) 
 	{
-       int a=30,b=40,c,d=5;
+       int a=60,b=800,c,d=5;
        c=a+d;
        
        System.out.println("add is "+c);
