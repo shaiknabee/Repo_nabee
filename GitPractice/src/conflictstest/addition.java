fc9e3a4dbc42d095832ecdd3bcc44832467b08c8
shaiknabee <138884531+shaiknabee@users.noreply.github.com>
2023-07-11 16:36:29 +0530

    commit8

--- GitPractice/src/conflictstest/addition.java
+++ GitPractice/src/conflictstest/addition.java
@@ -4,8 +4,9 @@ public class addition
 {
 	public static void main(String[] args) 
 	{
-       int x=30,y=40,c;
-       c=x+y;
+       int x=30,y=40,c,d=5;
+       c=x+d;
+       
        System.out.println("add is "+c);
 	}
 }
