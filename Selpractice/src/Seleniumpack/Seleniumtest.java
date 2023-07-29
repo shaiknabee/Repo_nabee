d0f049a21a58bcb12397697ee026368275d32d27
shaiknabee <138884531+shaiknabee@users.noreply.github.com>
2023-07-29 10:26:31 +0530

    commit

---
+++ Selpractice/src/Seleniumpack/Seleniumtest.java
@@ -0,0 +1,12 @@
+package Seleniumpack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Seleniumtest 
+{
+	public static void main(String[] args) 
+	{
+     WebDriver d=new ChromeDriver();
+	}
+}
